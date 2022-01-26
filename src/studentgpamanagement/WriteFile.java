dcbd127d3cd2418d6d0088ba2d6a227d047ea2e3
giangkoi01 <giangtvph10575@fpt.edu.vn>
2022-01-26 16:09:48 +0700

    studentmanage

---
+++ src/studentgpamanagement/WriteFile.java
@@ -0,0 +1,15 @@
+package studentgpamanagement;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+public class WriteFile {
+	public void writeStudentFile(Student[] students) {
+		try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream("StudentList.txt", true))) {
+            outputStream.writeObject(students);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+	}
+}
